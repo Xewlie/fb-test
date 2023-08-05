96018bca37ed853dfdcc3456f913d9fa0d3f5ab3
HyperXewl <stefanbuzianczuk@gmail.com>
2023-08-06 09:57:37 +1000

    even more setup (buttons to test crash/init code)

---
+++ Assets/Scripts/CrashButtonScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CrashButtonScript : MonoBehaviour
+{
+    // Text that will appear on the button
+    private string buttonText = "Cause Crash!";
+
+    void OnGUI()
+    {
+        GUIStyle myButtonStyle = new GUIStyle(GUI.skin.button);
+        myButtonStyle.fontSize = 25;
+
+        // Calculate button position and size
+        float buttonWidth = 200;
+        float buttonHeight = 70;
+        float buttonX = (Screen.width / 2) - (buttonWidth / 2);
+        float buttonY = (Screen.height / 2) - (buttonHeight / 2);
+
+        // Create a rect for button
+        Rect buttonRect = new Rect(buttonX, buttonY, buttonWidth, buttonHeight);
+
+        // Create a button and add a function to perform when it's clicked
+        if (GUI.Button(buttonRect, buttonText, myButtonStyle))
+        {
+            // Force a native crash for testing Crashlytics
+            throw new System.Exception("(ignore) this is a test crash");
+        }
+    }
+}
---
+++ Assets/Scripts/CrashlyticsInitializer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Analytics;
+using Firebase.Crashlytics;
+
+public class CrashlyticsInitializer : MonoBehaviour
+{
+    private void Awake()
+    {
+        CheckForDependencies();
+    } 
+
+    private void CheckForDependencies()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => 
+        {
+            DependencyStatus dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                Debug.Log("Firebase dependencies are available.");
+                InitializeFirebase();
+            }
+            else
+            {
+                Debug.LogError($"Could not resolve all Firebase dependencies: {dependencyStatus}");
+            }
+        });
+    }
+
+    protected virtual void InitializeFirebase()
+    {
+        FirebaseApp app = FirebaseApp.DefaultInstance;
+
+        if(app != null)
+        {
+            Debug.Log("Firebase initialized successfully.");
+
+            // Initialize Crashlytics
+            Crashlytics.SetUserId(app.Options.ProjectId);
+
+            if(Crashlytics.IsCrashlyticsCollectionEnabled)
+            {
+                Debug.Log("Crashlytics has been enabled.");
+            }
+            else
+            {
+                Debug.LogError("Couldn't enable Crashlytics.");
+            }
+            
+            // Log an event for Firebase Analytics
+            FirebaseAnalytics.LogEvent("Firebase_initialized", "Firebase", "Firebase and Crashlytics initialized successfully"); 
+        }
+        else
+        {
+            Debug.LogError("Failed to initialize Firebase.");
+        }
+    }
+}
